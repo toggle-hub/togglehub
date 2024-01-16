c878faec2e4f38c551e5ffb95e7fff0d244b0cee
xsadia <felipe.rosa.arthur27@gmail.com>
2024-01-16 10:03:55 -0300

    feat: setup email verification worker

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,15 +1,30 @@
-import { Duration, Stack, StackProps } from "aws-cdk-lib";
+import { Stack, StackProps } from "aws-cdk-lib";
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as sqs from "aws-cdk-lib/aws-sqs";
+import * as eventSources from "aws-cdk-lib/aws-lambda-event-sources";
 import { Construct } from "constructs";
 
 export class InfraStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const hello = new lambda.Function(this, "HelloHandler", {
-      runtime: lambda.Runtime.NODEJS_18_X,
-      code: lambda.Code.fromAsset("lambda"),
-      handler: "hello.handler",
+    const emailQueue = new sqs.Queue(this, "EmailVerificationQueue", {
+      queueName: "EmailVerificationQueue",
     });
+
+    const emailVerificationHandler = new lambda.Function(
+      this,
+      "EmailVerificationHandler",
+      {
+        runtime: lambda.Runtime.NODEJS_20_X,
+        code: lambda.Code.fromAsset("dist"),
+        functionName: "EmailVerificationHandler",
+        handler: "emailVerification.handler",
+      }
+    );
+
+    const eventSource = new eventSources.SqsEventSource(emailQueue);
+
+    emailVerificationHandler.addEventSource(eventSource);
   }
 }
